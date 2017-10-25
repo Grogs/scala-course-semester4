c5aaed3302f52571bccea4c48925ab42e20106f6
Greg Dorrell <grdorrell@hotels.com>
2017-10-25 18:24:24 +0100

    Lesson 6 implementation

--- client/src/main/scala/fss/App.scala
+++ client/src/main/scala/fss/App.scala
@@ -1,9 +1,12 @@
 package fss
 
-import org.scalajs.dom.document
+import org.scalajs.dom.{Event, document}
 import org.scalajs.dom.html.{Button, Input}
+import services.hotels.HotelsService
 
-import scala.scalajs.js.JSApp
+import autowire._
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("App")
@@ -17,8 +20,39 @@ object App {
   @JSExport
   def main(): Unit = {
     println("Hello from Scala.js")
-    //This code runs when the page loads
+
+    //EXERCISE 1
+
+    //Key up for when the user changes the text
+    destination().onkeyup = handleChange _
+    distance().onkeyup = handleChange _
+
+    //On change for when the user selects an item from the autocomplete
+    distance().onchange = _ => reload(destination().value, distance().value.toDouble) //to show you can use a lambda instead
+
+    searchButton().style.display = "none"
+
+
+    //EXERCISE 2
+    new Autocomplete(
+      destination(),
+      Seq("London", "Paris", "Bath"),
+      _ => handleChange(null)
+    )
   }
+
+  def handleChange(e: Event) = {
+    reload(destination().value, distance().value.toDouble)
+  }
+
+  def reload(destination: String, distance: Double) = {
+    for {
+      hotels <- Client[HotelsService].search(destination, distance).call() //Note the .call()
+      table = views.html.hotelsTable(hotels).body //Yay, reused code across frontend and backend!
+    } hotelsTables().outerHTML = table
+  }
+
+  //For exercise 3, add `if hotels.nonEmpty` to the for-comprehension.
 
 }
 

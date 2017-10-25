c2dd6e8e5d5f2eecda6ff63f0e2f9c9f0b2c9620
Greg Dorrell <grdorrell@hotels.com>
2017-10-25 18:29:29 +0100

    Add imports needed for the exercises...

--- client/src/main/scala/fss/App.scala
+++ client/src/main/scala/fss/App.scala
@@ -1,9 +1,12 @@
 package fss
 
-import org.scalajs.dom.document
+import org.scalajs.dom.{Event, document}
 import org.scalajs.dom.html.{Button, Input}
+import services.hotels.HotelsService
 
-import scala.scalajs.js.JSApp
+import autowire._
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("App")
